71cf90f206ebad332987a9e8b0a0da547ebb6a8c
Vitaliy Fadeev <vital.fadeev@gmail.com>
2023-09-17 10:26:31 +0700

    upd

--- source/db.d
+++ source/db.d
@@ -1,3 +1,13 @@
 module db;
 
 // 9x9  -> 3x3  -> 1x1
+//
+// record
+//   reduce record
+//     reduce record
+//       reduced -> index
+//
+// index
+//   reduced
+//     reduced
+//       record
